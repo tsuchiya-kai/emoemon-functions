d50358ec717394bd3db8b8f42e6c627e57b9270d
tsuchiya-kai <yukaigangan@gmail.com>
2022-09-23 17:53:10 +0900

    関数を実装

--- pages/fuctions/average/average.ts
+++ pages/fuctions/average/average.ts
@@ -1,15 +1,41 @@
 import { Emotion } from "./type";
 
 export const totalCalculator = (emotions: Emotion[]): Emotion => {
+  const initial = {
+    angry: 0,
+    disgusted: 0,
+    fearful: 0,
+    happy: 0,
+    neutral: 0,
+    sad: 0,
+    surprised: 0,
+  };
+
+  const sumEmotion = emotions.reduce((result, current) => {
+    return {
+      angry: result.angry + current.angry,
+      disgusted: result.disgusted + current.disgusted,
+      fearful: result.fearful + current.fearful,
+      happy: result.happy + current.happy,
+      neutral: result.neutral + current.neutral,
+      sad: result.sad + current.sad,
+      surprised: result.surprised + current.surprised,
+    };
+  }, initial);
+
+  const averagedEmotion = {
+    angry: sumEmotion.angry / emotions.length,
+    disgusted: sumEmotion.disgusted / emotions.length,
+    fearful: sumEmotion.fearful / emotions.length,
+    happy: sumEmotion.happy / emotions.length,
+    neutral: sumEmotion.neutral / emotions.length,
+    sad: sumEmotion.sad / emotions.length,
+    surprised: sumEmotion.surprised / emotions.length,
+  };
+
   return {
-    angry: 0.7955375909805298,
-    disgusted: 0.005695052444934845,
-    fearful: 0.00017332482093479484,
-    happy: 0.0003722251858562231,
-    neutral: 0.1963672637939453,
-    sad: 0.0008870384772308171,
-    surprised: 0.0009675621404312551,
-    roomId: 1,
+    ...averagedEmotion,
+    roomId: emotions[0].roomId,
     createdAt: new Date(),
   };
 };
